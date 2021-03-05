4b5acf71d723fbf2bdf010588f7d4c166bc9664a
Gino Liberati <ginoliberati97@gmail.com>
2021-03-05 10:17:28 -0300

    auth

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class ItemController extends Controller
 {
-    //
+    public function show($item_id) {
+        $item = Items::where('id', $item_Id)->get();
+        return view();
+    }
+
+    public function edit($item_Id) {
+        $userId = auth('api')->user()->id;
+        $item = Items::where('id', $item_Id)
+            ->update();
+    }
 }
