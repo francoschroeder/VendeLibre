9bcaf2cdcf4784670bc05fe5d4fe76f0b3e6a2b9
Franco Schroeder <yofrancoschroeder@yahoo.com.ar>
2021-03-10 10:59:20 -0300

    Material-UI

--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Store from './Store'
+import Album from './Album'
 
 class App extends Component {
     render () {
     return (
         <BrowserRouter>
             <Switch>
-                <Route path="/" component = { Store }/>
+                <Route path="/" component = { Album }/>
             </Switch>
         </BrowserRouter>
     )
