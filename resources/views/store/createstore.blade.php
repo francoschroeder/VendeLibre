50d4a7adaf0a22199137c18ada7d0b639dea0953
Gino Liberati <ginoliberati97@gmail.com>
2021-03-11 12:01:51 -0300

    vista create store

--- resources/views/store/createstore.blade.php
+++ resources/views/store/createstore.blade.php
@@ -1,59 +1,83 @@
 @extends('layouts.app')
+<link href="https://fonts.googleapis.com/css?family=Muli:300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="{{ asset('css/bootstrap.min.css')}}" type="text/css">
+<link rel="stylesheet" href="{{ asset('css/font-awesome.min.css')}}" type="text/css">
+<link rel="stylesheet" href="{{ asset('css/themify-icons.css')}}" type="text/css">
+<link rel="stylesheet" href="{{ asset('css/elegant-icons.css')}}" type="text/css">
+<link rel="stylesheet" href="{{ asset('css/owl.carousel.min.css')}}" type="text/css">
+<link rel="stylesheet" href="{{ asset('css/nice-select.css')}}" type="text/css">
+<link rel="stylesheet" href="{{ asset('css/jquery-ui.min.css')}}" type="text/css">
+<link rel="stylesheet" href="{{ asset('css/slicknav.min.css')}}" type="text/css">
+<link rel="stylesheet" href="{{ asset('css/style.css')}}" type="text/css">
 
 @section('title')
     Añadir Tienda
 @endsection
 
 @section('content')
-<div class = "container">
-	<form method = "POST" action="/createStore">
+<section class="checkout-section spad">
+	
 						
 		@csrf
-		<div class = "container">
-			<div class="form-group row">
-				<label for="example-text-input" class="col-2 col-form-label">Nombre</label>
-				<div class="col-10">
-					<input class="form-control" type="text" id="example-text-input" name = "name">
+
+		 <!-- Shopping Cart Section Begin -->
+		
+        <div class="container">
+			<form method = "POST" action="/createStore">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <h3>Datos del Negocio</h3>
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <label for="fir">Nombre de la Tienda<span></span></label>
+                                <input class="form-control" type="text" id="example-text-input" name = "name">
+							</div>
+							<div class="col-lg-12">
+								<label for="example-text-input">Descripcion</label>
+								<input class="form-control" type="text" id="example-float-input" name = "description">
+							</div>
+                            <div class="col-lg-12">
+                                <label for="street">Direccion<span></span></label>
+                               <input class="form-control" type="text" id="example-float-input" name = "direction">
+                            
+                            </div>
+                            <div class="col-lg-12">
+                                <label for="email">Email<span></span></label>
+                                <input class="form-control" type="text" id="example-float-input" name = "email">
+                            </div>
+                            <div class="col-lg-12">
+                                <label for="phone">Telefono<span></span></label>
+                                <input class="form-control" type="text" id="example-float-input" name = "phone">
+							</div>
+							<div class="col-lg-12">
+								<label for="example-text-input" >Longitud</label>
+								<input class="form-control" type="float" id="example-float-input" name = "longitud">
+							</div>
+							<div class="col-lg-12">
+								<label for="example-text-input" >Latitud</label>
+								<input class="form-control" type="float" id="example-float-input" name = "latitud">
+							</div>
+							
+                        </div>
+                    </div>
 				</div>
-				<label for="example-text-input" class="col-2 col-form-label">Descripcion</label>
-				<div class="col-10">
-					<input class="form-control" type="float" id="example-float-input" name = "description">
+				<div>
+					<button type="submit" class="btn btn-primary">Agregar</button>
 				</div>
-				<label for="example-text-input" class="col-2 col-form-label">Telefono</label>
-				<div class="col-10">
-					<input class="form-control" type="float" id="example-float-input" name = "phone">
-				</div>
-				<label for="example-text-input" class="col-2 col-form-label">Email de contacto</label>
-				<div class="col-10">
-					<input class="form-control" type="float" id="example-float-input" name = "email">
-				</div>
-				<label for="example-text-input" class="col-2 col-form-label">Direccion</label>
-				<div class="col-10">
-					<input class="form-control" type="float" id="example-float-input" name = "direction">
-				</div>
-				<label for="example-text-input" class="col-2 col-form-label">Longitud</label>
-				<div class="col-10">
-					<input class="form-control" type="float" id="example-float-input" name = "longitud">
-				</div>
-				<label for="example-text-input" class="col-2 col-form-label">Latitud</label>
-				<div class="col-10">
-					<input class="form-control" type="float" id="example-float-input" name = "latitud">
-				</div>
-			</div>
-		</div>
-		<div>
-			<button type="submit" class="btn btn-primary">Agregar</button>
-		</div>
 										
-		<div>
-			<ul>
-				@foreach($errors->all() as $error)
-					<br>
-						<li>{{$error}}</li>
-				@endforeach	
-			</ul>
-		</div>
-	</form>
-</div>
+				<div>
+				<ul>
+					@foreach($errors->all() as $error)
+						<br>
+							<li>{{$error}}</li>
+					@endforeach	
+					</ul>
+				</div>
+			
+            </form>
+        </div>
+</section>
+<!-- Shopping Cart Section End -->
+
 
 @endsection
