8e340d167f85f7ce7455ac5d2708dcf48cbb883b
Gino Liberati <ginoliberati97@gmail.com>
2021-03-12 11:13:57 -0300

    navBar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,68 +19,91 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Muli:300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css')}}" type="text/css">
+    <link rel="stylesheet" href="{{ asset('css/font-awesome.min.css')}}" type="text/css">
+    <link rel="stylesheet" href="{{ asset('css/themify-icons.css')}}" type="text/css">
+    <link rel="stylesheet" href="{{ asset('css/elegant-icons.css')}}" type="text/css">
+    <link rel="stylesheet" href="{{ asset('css/owl.carousel.min.css')}}" type="text/css">
+    <link rel="stylesheet" href="{{ asset('css/nice-select.css')}}" type="text/css">
+    <link rel="stylesheet" href="{{ asset('css/jquery-ui.min.css')}}" type="text/css">
+    <link rel="stylesheet" href="{{ asset('css/slicknav.min.css')}}" type="text/css">
+    <link rel="stylesheet" href="{{ asset('css/style.css')}}" type="text/css">
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+           <!-- Header Section Begin -->
+    <header class="header-section">
+            <div class="header-top">
+                <div class="container">
+                    <div class="ht-left">
+                        <div class="mail-service">
+                            <i class=" fa fa-envelope"></i>
+                            hello.colorlib@gmail.com
+                        </div>
+                        <div class="phone-service">
+                            <i class=" fa fa-phone"></i>
+                            +65 11.188.888
+                        </div>
+                    </div>
+                    <div class="ht-right">
+                        <div>
+                            <ul class="navbar-nav ml-auto">
+                                    <!-- Authentication Links -->
+                                    @guest
+                                        @if (Route::has('login'))
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                            </li>
+                                        @endif
+                                        
+                                        @if (Route::has('register'))
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                            </li>
+                                        @endif
+                                    @else
+                                        <li class="nav-item dropdown">
+                                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                                {{ Auth::user()->name }}
+                                            </a>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+                                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                                onclick="event.preventDefault();
+                                                                document.getElementById('logout-form').submit();">
+                                                    {{ __('Logout') }}
+                                                </a>
 
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
-                            
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
+                                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                                    @csrf
+                                                </form>
+                                            </div>
+                                        </li>
+                                    @endguest
+                                </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </nav>
+            @yield('navBar')
+    </header>
+        <!-- Header End -->
 
-        <main class="py-4">
+    <main class="py-4">
             @yield('content')
-        </main>
+    </main>
     </div>
 
-
+    <script src="{{ asset('js/jquery-3.3.1.min.js')}}"></script>
+    <script src="{{ asset('js/bootstrap.min.js')}}"></script>
+    <script src="{{ asset('js/jquery-ui.min.js')}}"></script>
+    <script src="{{ asset('js/jquery.countdown.min.js')}}"></script>
+    <script src="{{ asset('js/jquery.nice-select.min.js')}}"></script>
+    <script src="{{ asset('js/jquery.zoom.min.js')}}"></script>
+    <script src="{{ asset('js/jquery.dd.min.js')}}"></script>
+    <script src="{{ asset('js/jquery.slicknav.js')}}"></script>
+    <script src="{{ asset('js/owl.carousel.min.js')}}"></script>
+    <script src="{{ asset('js/main.js')}}"></script>
 </body>
 </html>
--- resources/views/store/showstore.blade.php
+++ resources/views/store/showstore.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.navBar')
 
 @section('title')
     Página Principal
