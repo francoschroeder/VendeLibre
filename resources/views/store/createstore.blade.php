b3663e2c6824fb91550945ddf4ba9f724065bd28
Gino Liberati <ginoliberati97@gmail.com>
2021-03-12 11:55:59 -0300

    listo colores

--- resources/views/store/createstore.blade.php
+++ resources/views/store/createstore.blade.php
@@ -1,15 +1,4 @@
 @extends('layouts.app')
-<link href="https://fonts.googleapis.com/css?family=Muli:300,400,500,600,700,800,900&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="{{ asset('css/bootstrap.min.css')}}" type="text/css">
-<link rel="stylesheet" href="{{ asset('css/font-awesome.min.css')}}" type="text/css">
-<link rel="stylesheet" href="{{ asset('css/themify-icons.css')}}" type="text/css">
-<link rel="stylesheet" href="{{ asset('css/elegant-icons.css')}}" type="text/css">
-<link rel="stylesheet" href="{{ asset('css/owl.carousel.min.css')}}" type="text/css">
-<link rel="stylesheet" href="{{ asset('css/nice-select.css')}}" type="text/css">
-<link rel="stylesheet" href="{{ asset('css/jquery-ui.min.css')}}" type="text/css">
-<link rel="stylesheet" href="{{ asset('css/slicknav.min.css')}}" type="text/css">
-<link rel="stylesheet" href="{{ asset('css/style.css')}}" type="text/css">
-
 @section('title')
     Añadir Tienda
 @endsection
