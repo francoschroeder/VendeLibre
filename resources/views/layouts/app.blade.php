a8db52492d9a95e4e8e6b6a6d3a8acf06a49fbec
Gino Liberati <ginoliberati97@gmail.com>
2021-03-12 11:34:04 -0300

    arreglo de barra con el mismo color

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,8 @@
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    
+    
    
 
     <!-- Styles -->
@@ -52,18 +54,18 @@
                                     <!-- Authentication Links -->
                                     @guest
                                         @if (Route::has('login'))
-                                            <li class="nav-item">
+                                            <li class="nav-item-2">
                                                 <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                                             </li>
                                         @endif
                                         
                                         @if (Route::has('register'))
-                                            <li class="nav-item">
+                                            <li class="nav-item-2">
                                                 <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                                             </li>
                                         @endif
                                     @else
-                                        <li class="nav-item dropdown">
+                                        <li class="nav-item-2 dropdown">
                                             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                                 {{ Auth::user()->name }}
                                             </a>
