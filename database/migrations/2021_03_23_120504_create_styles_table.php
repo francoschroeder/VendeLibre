385929dfa973b6f5995dcda2a307e641cee2630b
Franco Schroeder <yofrancoschroeder@yahoo.com.ar>
2021-03-23 09:14:03 -0300

    Tabla de Estilos

---
+++ database/migrations/2021_03_23_120504_create_styles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStylesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('styles', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('store_id');
+            $table->foreign('store_id')
+                    ->references('id')
+                    ->on('stores')
+                    ->onDelete('cascade');
+            $table->integer('item_style');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('styles');
+    }
+}
