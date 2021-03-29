c600e490184ff581efcbf13b1230ba54e671f0bb
Gino Liberati <ginoliberati97@gmail.com>
2021-03-29 17:09:18 -0300

    Update ItemCard.js

--- resources/js/components/secondaryComponent/ItemCard.js
+++ resources/js/components/secondaryComponent/ItemCard.js
@@ -94,7 +94,7 @@ export default function ItemCard({item, edit}) {
         <Button size="small" color="primary" onClick={() => setEditable(true)}>
           Editar
         </Button>
-        <Button size="small" color="primary" onClick={()=>guardarCambios(id,item.id)}>
+        <Button size="small" color="primary" onClick={()=>eliminarItem(id,item.id)}>
          Eliminar
         </Button>
       </CardActions>
@@ -128,7 +128,7 @@ export default function ItemCard({item, edit}) {
     e.target.src="/images/placeholder.jpg";
   }
 
-  function guardarCambios(id,itemId) {
+  function eliminarItem(id,itemId) {
     axios.delete('/store/'+id+'/'+itemId+'/delete')
         .then(function(response) {
             console.log(response);
