9c346451d1bf9fc5825628a5433eb8023b39d82a
Franco Schroeder <yofrancoschroeder@yahoo.com.ar>
2021-04-01 10:16:12 -0300

    Error por background corregido

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
 @section('content')
 <section class="hero-section">
         <div class="hero-items owl-carousel">
-            <div class="single-hero-items set-bg" data-setbg="img/hero-1.jpg">
+            <div class="single-hero-items set-bg">
                 <div class="container">
                     <div class="row">
                         <div class="col-lg-5">
