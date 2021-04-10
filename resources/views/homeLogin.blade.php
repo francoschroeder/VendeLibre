52e3087bd183410a330961819ac2be350856e641
Franco Schroeder <yofrancoschroeder@yahoo.com.ar>
2021-04-10 19:23:17 -0300

    Update homeLogin.blade.php

--- resources/views/homeLogin.blade.php
+++ resources/views/homeLogin.blade.php
@@ -4,7 +4,7 @@
 <div class="row">
     <div class="col-lg-3 col-md-6 col-sm-8 order-2 order-lg-1 produts-sidebar-filter">
         <div class="filter-widget">
-            <a href="https://auth.mercadopago.com.ar/authorization?client_id=3364711930325075&response_type=code&platform_id=mp&redirect_uri=https://vende-libre.herokuapp.com/vincular"><button>Vincular con MercadoPago</button></a>
+            <a href="https://auth.mercadopago.com.ar/authorization?client_id=3364711930325075&response_type=code&platform_id=mp&redirect_uri=https%3A%2F%2Fvende-libre.herokuapp.com/vincular"><button>Vincular con MercadoPago</button></a>
             <h4 class="fw-title">Mis Tiendas</h4>
                         
             @forelse($stores as $store )
